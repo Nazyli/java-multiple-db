476f124cdfd79f081c41f5104b23b4447afa5870
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-02-07 08:48:45 +0700

    initial-project

---
+++ src/test/java/com/nazyli/multipledb/MultipleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazyli.multipledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
